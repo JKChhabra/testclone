641a36dae3ceda930d03399b501836e4e5a80901
jagmeet@prophecy.io <jagmeet@prophecy.io>
2023-03-31 12:41:48 +0000

    Cloning prophecy project main branch

---
+++ README.md
@@ -0,0 +1,13 @@
+# HelloWorld Repository
+
+*** Release notes for version: 1.0 ***
+
+releasing
+
+*** Release notes for version: v1.0 ***
+
+releasing my first job for databricks.
+
+*** Release notes for version: v1.1 ***
+
+updating 1.1
---
+++ pipelines/report_top_customers/code/job/graph/Report.py
@@ -0,0 +1,17 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.libs import typed_lit
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Report(spark: SparkSession, in0: DataFrame):
+    in0.write\
+        .option("header", True)\
+        .option("sep", ",")\
+        .option("ignoreLeadingWhiteSpace", True)\
+        .option("ignoreTrailingWhiteSpace", True)\
+        .mode("overwrite")\
+        .option("separator", ",")\
+        .option("header", True)\
+        .csv("dbfs:/Prophecy/jagmeet@prophecy.io/report-top-customers.csv")
